f5521d8c86e94a1ea662dde8c6da2c487edb8a35
Zoe Roux <zoe.roux@zoriya.dev>
2024-05-16 01:41:38 +0200

    Remove rnw from list of csjIntepor

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import reactNative from 'vite-plugin-react-native-web';
-import { cjsInterop } from 'vite-plugin-cjs-interop';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import reactNative from "vite-plugin-react-native-web";
+import { cjsInterop } from "vite-plugin-cjs-interop";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    reactNative(),
-    cjsInterop({
-      dependencies: [
-        'inline-style-prefixer',
-        'inline-style-prefixer/**',
-        'react-native-web',
-      ],
-    }),
-  ],
+	plugins: [
+		react(),
+		reactNative(),
+		cjsInterop({
+			dependencies: [
+				"inline-style-prefixer",
+				"inline-style-prefixer/**",
+			],
+		}),
+	],
 });
